7821ae5428cd75ee8827b670f2ad8201f5e49676
Shantanu Kumar <kumar.shantanu@gmail.com>
2016-12-18 18:22:00 +0530

    bump citius version to 0.2.4

--- project.clj
+++ project.clj
@@ -18,7 +18,7 @@
              :c19 {:dependencies [[org.clojure/clojure "1.9.0-alpha14"]]
                    :global-vars {*unchecked-math* :warn-on-boxed}}
              :dln {:jvm-opts ["-Dclojure.compiler.direct-linking=true"]}
-             :perf {:dependencies [[citius "0.2.2"]
+             :perf {:dependencies [[citius "0.2.4"]
                                    [org.clojure/java.jdbc "0.4.2"]]
                     :test-paths ["perf"]}}
   :jvm-opts ^:replace ["-server" "-Xms2048m" "-Xmx2048m"]
