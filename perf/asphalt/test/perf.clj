92fa5202f6dc69ea666b47058c858b18dd01f0fb
Shantanu Kumar <kumar.shantanu@gmail.com>
2017-01-06 01:21:37 +0530

    omit Asphalt-map variant from perf benchmarks as we dropped map format support

--- perf/asphalt/test/perf.clj
+++ perf/asphalt/test/perf.clj
@@ -18,7 +18,7 @@
     [asphalt.type      :as t]))
 
 
-(use-fixtures :once (c/make-bench-wrapper ["clojure.java.jdbc" "Asphalt-SQL" "Asphalt-template" "Asphalt-map"]
+(use-fixtures :once (c/make-bench-wrapper ["clojure.java.jdbc" "Asphalt-SQL" "Asphalt-template"]
                       {:chart-title "Clojure.java.jdbc vs Asphalt"
                        :chart-filename (format "bench-clj-%s.png" c/clojure-version-str)}))
 
@@ -50,13 +50,6 @@
 (def ^String s-delete (:sql t-delete))
 (def ^String s-select (:sql t-select))
 
-(def m-insert {:sql s-insert
-               :param-keys [:name :salary :dept]
-               :param-types (apply vector-of :byte (:param-types t-insert))})
-(def m-delete {:sql s-delete})
-(def m-select {:sql s-select
-               :result-types (apply vector-of :byte (:result-types t-select))})
-
 
 (deftest bench-insert-delete
   (testing "Insert and delete"
@@ -75,10 +68,7 @@
              (a/update conn s-delete nil))
            (do
              (a/update conn t-insert data)
-             (a/update conn t-delete nil))
-           (do
-             (a/update conn m-insert data)
-             (a/update conn m-delete nil))))))))
+             (a/update conn t-delete nil))))))))
 
 
 (deftest bench-select
@@ -94,13 +84,11 @@
           (c/compare-perf "select-row"
             (jdbc/query db-con [s-select])
             (a/query a/fetch-rows conn s-select nil)
-            (a/query a/fetch-rows conn t-select nil)
-            (a/query a/fetch-rows conn m-select nil))))
+            (a/query a/fetch-rows conn t-select nil))))
       ;; bench c.j.j `:as-arrays? true` with asphalt
       (jdbc/with-db-connection [db-con db-spec]
         (i/with-connection [conn u/orig-ds]
           (c/compare-perf "select-row-as-arrays"
             (jdbc/query db-con [s-select] {:as-arrays? true})
             (a/query a/fetch-rows conn s-select nil)
-            (a/query a/fetch-rows conn t-select nil)
-            (a/query a/fetch-rows conn m-select nil)))))))
+            (a/query a/fetch-rows conn t-select nil)))))))
