2cf4990b88aa541086e065069f808aff9b43a9dd
Shantanu Kumar <kumar.shantanu@gmail.com>
2017-02-20 11:49:25 +0530

    strip the prefix in test connection-pool profile names

--- project.clj
+++ project.clj
@@ -22,10 +22,10 @@
              :perf {:dependencies [[citius "0.2.4"]
                                    [org.clojure/java.jdbc "0.7.0-alpha1"]]
                     :test-paths ["perf"]}
-             :cp-dbcp  {:test-paths ["test-connpool/dbcp"]  :dependencies [[clj-dbcp "0.8.2"]]}
-             :cp-dbcp2 {:test-paths ["test-connpool/dbcp2"] :dependencies [[clj-dbcp "0.9.0"]]}
-             :cp-c3p0  {:test-paths ["test-connpool/c3p0"]  :dependencies [[com.mchange/c3p0 "0.9.5.2"]]}
-             :cp-bone  {:test-paths ["test-connpool/bone"]  :dependencies [[com.jolbox/bonecp "0.8.0.RELEASE"]]}}
+             :dbcp  {:test-paths ["test-connpool/dbcp"]  :dependencies [[clj-dbcp "0.8.2"]]}
+             :dbcp2 {:test-paths ["test-connpool/dbcp2"] :dependencies [[clj-dbcp "0.9.0"]]}
+             :c3p0  {:test-paths ["test-connpool/c3p0"]  :dependencies [[com.mchange/c3p0 "0.9.5.2"]]}
+             :bone  {:test-paths ["test-connpool/bone"]  :dependencies [[com.jolbox/bonecp "0.8.0.RELEASE"]]}}
   :jvm-opts ^:replace ["-server" "-Xms2048m" "-Xmx2048m" #_FIXME "-Duser.timezone=UTC"]
   :global-vars {*warn-on-reflection* true
                 *assert* true})
