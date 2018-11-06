c209601639290bb1881b5424430c28c2350639a0
Shantanu Kumar <kumar.shantanu@gmail.com>
2018-11-06 12:10:56 +0530

    update project config for Clojure 1.7 baseline

--- project.clj
+++ project.clj
@@ -5,19 +5,19 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :java-source-paths ["java-src"]
   :javac-options ["-target" "1.7" "-source" "1.7" "-Xlint:-options"]
+  :global-vars {*assert* true
+                *warn-on-reflection* true
+                *unchecked-math* :warn-on-boxed}
   :profiles {:dev {:dependencies [[org.clojure/tools.nrepl "0.2.12"]
                                   [simple-jndi "0.11.4.1"]
                                   [com.h2database/h2 "1.3.176"]]}
-             :provided {:dependencies [[org.clojure/clojure "1.6.0"]
+             :provided {:dependencies [[org.clojure/clojure "1.7.0"]
                                        [clj-dbcp "0.8.2"]]
                         :test-paths ["test-connpool/dbcp"]}
-             :c16 {:dependencies [[org.clojure/clojure "1.6.0"]]}
-             :c17 {:dependencies [[org.clojure/clojure "1.7.0"]]
-                   :global-vars {*unchecked-math* :warn-on-boxed}}
-             :c18 {:dependencies [[org.clojure/clojure "1.8.0"]]
-                   :global-vars {*unchecked-math* :warn-on-boxed}}
-             :c19 {:dependencies [[org.clojure/clojure "1.9.0"]]
-                   :global-vars {*unchecked-math* :warn-on-boxed}}
+             :c07 {:dependencies [[org.clojure/clojure "1.7.0"]]}
+             :c08 {:dependencies [[org.clojure/clojure "1.8.0"]]}
+             :c09 {:dependencies [[org.clojure/clojure "1.9.0"]]}
+             :c10 {:dependencies [[org.clojure/clojure "1.10.0-beta4"]]}
              :dln {:jvm-opts ["-Dclojure.compiler.direct-linking=true"]}
              :perf {:dependencies [[citius "0.2.4"]
                                    [org.clojure/java.jdbc "0.7.1"]]
@@ -26,6 +26,4 @@
              :dbcp2 {:test-paths ["test-connpool/dbcp2"] :dependencies [[clj-dbcp "0.9.0"]]}
              :c3p0  {:test-paths ["test-connpool/c3p0"]  :dependencies [[com.mchange/c3p0 "0.9.5.2"]]}
              :bone  {:test-paths ["test-connpool/bone"]  :dependencies [[com.jolbox/bonecp "0.8.0.RELEASE"]]}}
-  :jvm-opts ^:replace ["-server" "-Xms2048m" "-Xmx2048m" #_FIXME "-Duser.timezone=UTC"]
-  :global-vars {*warn-on-reflection* true
-                *assert* true})
+  :jvm-opts ^:replace ["-server" "-Xms2048m" "-Xmx2048m" #_FIXME "-Duser.timezone=UTC"])
